d80715e4adfae68e655cd980772db325c35f14d3
TugAh <tunganh97tuaf@gmail.com>
2024-09-29 19:02:46 +0700

    Framework_17_Switch_Page_Object

---
+++ testcases/com/nopcommerce/account/Level08_Switch_Page.java
@@ -0,0 +1,86 @@
+package com.nopcommerce.account;
+
+import commons.BaseTest;
+import commons.PageGenratorManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import pageObjects.CustomerPageObject;
+import pageObjects.HomePageObject;
+import pageObjects.LoginPageObject;
+import pageObjects.RegisterPageObject;
+
+public class Level08_Switch_Page extends BaseTest {
+    WebDriver driver;
+    private HomePageObject homePage;
+    private RegisterPageObject registerPage;
+    private CustomerPageObject customerPage;
+    private LoginPageObject loginPage;
+    private String emailAddress = getEmailRandom();
+
+
+    @Parameters("browser")
+    @BeforeClass
+    public void beforeClass(String browserName) {
+        driver = getBrowserDriver(browserName);
+
+        PageGenratorManager.getHomePage(driver);
+        // Mở ra URL page nào -> khởi tạo nó lên
+        // Từ 1 page này chuyển qua page kia -> Khở tạo page đó lên
+
+    }
+
+    @Test
+    public void User_01_Register_Success() {
+        homePage = registerPage.clickToNopcommerceLogo();
+        registerPage = homePage.clickToRegisterLink();
+
+        registerPage.enterToFirstNameTextBox("Tung");
+        registerPage.enterToLastNameTextBox("Anh");
+        registerPage.enterToEmailTextBox(emailAddress);
+        registerPage.enterToPasswordTextBox("123tunganh");
+        registerPage.enterToConfirmPasswordTextBox("123tunganh");
+
+        registerPage.clickToRegisterButton();
+
+        Assert.assertEquals(registerPage.getRegistrationCompletedMsg(), "Your registration completed");
+
+
+    }
+
+    @Test
+    public void User_02_Login_Success() {
+        homePage = registerPage.clickToLogOutLink();
+
+        loginPage = homePage.clickToLoginLink();
+
+
+        loginPage.enterToEmailTextbox(emailAddress);
+        loginPage.enterToPasswordTextbox("123tunganh");
+        homePage = loginPage.clickToLoginButton();
+        // valid data login > login success > Homepage
+        customerPage = homePage.clickToMyAccountLink();
+        // -> Customer page
+
+        //verify
+        Assert.assertEquals(customerPage.getFirstNameTextboxAttributeValue(), "Tung");
+        Assert.assertEquals(customerPage.getLastNameTextboxAttributeValue(), "Anh");
+        Assert.assertEquals(customerPage.getEmailAddressTextboxAttributeValue(), emailAddress);
+    }
+
+    @Test
+    public void User_03_Switch_Page(){
+
+    }
+
+
+
+    @AfterClass
+    public void afterClass() {
+        closeBrowser();
+    }
+
+}
